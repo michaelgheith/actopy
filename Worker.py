eda74b849b116ec9ec7fb39cc531458b0eb04c98
Michael Gheith <michael@juxtaploitation.com>
2017-09-12 18:38:32 -0500

    minor cleanup

--- Worker.py
+++ Worker.py
@@ -1,23 +1,21 @@
+#!/usr/bin/env python
+
 import threading
 import time
 
+
 #This is what gets spawned by the server when it receives a connection.
-
-
 class worker(threading.Thread):
     def __init__(self, sock):
         threading.Thread.__init__(self)
         self.sock = sock
 
     def run(self):
-
         print "Worker thread %s has started." % threading.current_thread()
-        #print threading.current_thread()
         
         data = self.sock.recv(1024)
         print "Received [%s]" % data
 
         self.sock.send("This note is from the worker thread %s on the server!" % threading.current_thread())
-        time.sleep(600)
 
         self.sock.close()
